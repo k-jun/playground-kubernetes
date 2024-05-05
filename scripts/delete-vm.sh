c200cd029993da5d63b0ff4316a9c3b879876308
k-jun <keijun091221@gmail.com>
2024-05-05 19:25:03 +0900

    feat(all): regular update

--- scripts/delete-vm.sh
+++ scripts/delete-vm.sh
@@ -1,4 +1,15 @@
-for i in 101 102 103 201 202 203 9002 9001; do
+for i in 102 202; do
+  ssh 192.168.11.12 qm shutdown $i
+  ssh 192.168.11.12 qm destroy $i
+done
+
+for i in 103 203; do
+  ssh 192.168.11.13 qm shutdown $i
+  ssh 192.168.11.13 qm destroy $i
+done
+
+for i in 101 201 9002 9001; do
   qm shutdown $i
   qm destroy $i
 done
+
